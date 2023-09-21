de1df8456e544fdcb7b401669f202d5fb1869ba6
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2023-09-21 06:35:59 +0100

    removing unused package and routes

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import shortUrl from "node-url-shortener";
-import bcrypt from "bcrypt";
 import cors from "cors";
 import mongoose from "mongoose";
-// import passport from "passport-google-oauth20";
-
 
 
 const app = express();
@@ -15,119 +11,89 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-const saltRounds = 10;
+mongoose.connect('mongodb+srv://admin-emmanuel:uche@cluster0.hexan43.mongodb.net/linkShorternerUserDB',
+{useNewUrlParser:true})
+.then(()=>console.log("connected to DB"))
+.catch(err=>console.log(err));
 
-mongoose.connect('mongodb+srv://admin-emmanuel:uche@cluster0.hexan43.mongodb.net/linkShorternerUserDB',{useNewUrlParser:true});
-
-const signUpUserSchema = new mongoose.Schema({ 
-    firstName: { type:String,
-    required:true
-},
-    lastName:{
-        type: String,
-        required: true,
+const shortlySchema = new mongoose.Schema({
+    longUrl:{
+        type:String,
+        required:[true, "Please provide a long Url"]
     },
-    
-    email:{
-        type: String,
-        required: [true, "Please provide an email address!"],
-        unique: [true, "Email already exist"]
+    shortId:{
+        type:String,
+        unique: [true, "ShortId already exist"]
     },
-
-    password:{
-        type: String,
-        required: [true, "please provide a password"]
-    },
-
-    registered: {
-        type: Date
+    createdAT:{
+        type:Date
     }
+    
 });
 
-const SignUpUser = mongoose.model("SignUpUser", signUpUserSchema);
+const Shortly = mongoose.model("Shortly", shortlySchema);
 
+
+//check url
+const validateUrl = (url)=>{
+    try{
+       let link = new URL(url);
+        if(link){
+            return true
+        } 
+    }catch(err){
+        console.log(err)
+        return false
+    }
+}
+
+const char_set = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+function max_random_number(max) {
+  return Math.floor(Math.random() * max);
+}
+function get_random_string(length) {
+  let random_string = '';
+  for(let i = 0; i < length; i++) {
+    random_string += char_set[max_random_number(char_set.length - 1)];
+  }
+  
+  return random_string;
+}
+
+let value = 5;
+const uniqueID = get_random_string(value)
 
 //shorten long url
 
 app.post('/shorten', async(req, res)=>{
     const {longUrl} = req.body;
-    if(longUrl === '' || !longUrl){
-        return res.status(400).json('Input link is empty')
-    } else{
-        await shortUrl.short(longUrl, (err, url)=>{
-            if(err){
-                res.status(404).json(err)
-            } else{
-               return res.status(200).json({
-                    longLink: longUrl,
-                    shortLink: url,
-                    date: new Date(),
-                })
-            }
+    if(longUrl === ''){
+        return res.status(400).json({message:'Input link is empty'})
+    } 
+    const checkUrl = validateUrl(longUrl);
+    if(checkUrl === false){
+        return res.status(400).json({message:"Invalid URL"});
+    }else{
+        let doesUrlExistInDB = await Shortly.findOne({longUrl:longUrl});
+        if(doesUrlExistInDB === null){
+            let saveURL = new Shortly({
+            longUrl:longUrl,
+            shortId: `short.ly/${uniqueID}`,
+            createdAt: new Date()
         })
+        saveURL.save();
+        let result = {
+            id:saveURL._id,
+            id:saveURL.shortId,
+            id:saveURL.longUrl,
+        }
+        console.log(result) 
+        return res.status(200).json(result)
+        }
     }
 });
 
 
-//login a user
-
-app.post('/login', (req, res)=>{
-    const {email, password} = req.body;
-         SignUpUser.findOne({email:email}, (err, foundUser)=>{
-        if(err){
-            console.log(err)
-        }
-        if(!foundUser){
-           return res.status(404).json("User not found")
-        }
-        if(foundUser){
-          bcrypt.compare(password, foundUser.password,  function(err, result){
-            if(err){
-                console.log(err)
-            }if(result === false){
-               return res.status(404).json("Incorrect username and password")
-            }if(result === true){
-               return res.status(200).json(foundUser.firstName)
-            }
-        });
-        }
-    })
-});
-
-// registering a user
-
-app.post('/signup', async(req, res)=>{
-     const { firstName, lastName, password, email} = req.body;
-     SignUpUser.findOne({email:email}, (err, FoundResult)=>{
-        if(err){
-            console.log(err)
-        }else if(FoundResult){
-          return res.status(404).json("User already exist")
-        }if(!FoundResult){
-            bcrypt.hash(password, saltRounds, function(err, hash) {
-        if(err){
-            console.log(err)
-        }else{
-            const signUpUser = new SignUpUser({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                registered: new Date(),
-                password: hash 
-               });
-               signUpUser.save(()=>{
-                if(err){
-                    console.log(err)
-                } else{
-                   return res.status(200).json("SignUp successful")
-                }
-            })   
-        }  
-     });
-        }
-     })
-        
-});
 
 app.listen(PORT, ()=>{
     console.log(`it's working on port ${PORT}`)
